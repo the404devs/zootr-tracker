fcc7b3b289dad6367842840a456c56fd9ae0ad4b
Owen Goodwin <the404devs@gmail.com>
2020-06-12 14:36:32 -0400

    test

--- js/classes/song.js
+++ js/classes/song.js
@@ -1,7 +1,7 @@
-define(["require", "classes/item", "data/ages", "data/items"], function(require, Item, Age){
-  return function Song(name, notes){
-  	this.notes = notes;
+define(["require", "data/items", "classes/item", "data/ages"], function(require, Item, Age) {
+    return function Song(name, notes) {
+        this.notes = notes;
 
-    return new Item(name, Age.ANY, [], {requirements: [require('data/items').FAIRY_OCARINA], notes:this.notes});
-  };
+        return new Item(name, Age.ANY, [], { requirements: [require('data/items').FAIRY_OCARINA], notes: this.notes });
+    };
 });
